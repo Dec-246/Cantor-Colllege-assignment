ea4d0be59931f70680a5c3f82391ed1907347d9b
Dec-246 <c3029680@my.shu.ac.uk>
2024-03-25 02:36:51 +0000

    Entire website

---
+++ Website_Code/index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link 
+    rel="stylesheet" 
+    href="css/mobile.css"  
+    type="text/css"
+    media="only screen"
+    >
+    <link
+    rel="stylesheet"
+    href="css/desktop.css"
+    type="text/css"
+    media="only screen and (min-width : 608px)"
+    />
+    <script> src="UI.js" </script>
+    </head>
+
+
+
+
+    <body>
+        <div class="container">
+
+        <header>
+            <div class="logo">
+                <a href="index.html">
+                <img 
+                class="image"
+                src="./Images/logo.jpg"
+                alt="logo for Cantor College" />
+                </a>
+            </div>
+            
+            <h1>Home Page</h1>
+
+                <div>
+                    <nav>
+                        <ul>        
+                        <li><a href="course.html"> Courses</a></li>
+                        <li><a href="facilities.html"> Facilities page</a></li>
+                        <li><a href="information.html"> Information page</a></li>
+                        <li><a href="contacts.html"> Contacts page</a></li>
+
+                        </ul>  
+                    </nav>
+                </div>
+            </header>
+
+
+            <main>
+                    <div class="introductionForHomePage">
+                        <p>
+                            Welcome to the official website for Cantor College;
+                            Here you will find all information concerning our:
+                            <ul>
+                                <li>Courses</li>
+                                <li>Facilities</li>
+                                <li>Information for students/ staff</li>
+                                <li>How we work with Businesses</li>
+                                <li>Contact Information</li>
+                            </ul>
+                            
+                        </p>
+                    </div>
+
+                <section>
+                    <div>
+                    <p>
+                The Cantor College is situated in the 
+                city centre of sheffield. The city campus 
+                is just a 10 minute walk from the heart 
+                of the city, meaning students have quick 
+                and easy access to shops and Cafés in 
+                their free periods. In addition to this, 
+                the Cantor College has integrated the modern 
+                and diverse cultural dynamics of the city:
+                Therefore, no matter your background, you are 
+                bound to be accepted and make friends within 
+                our College community.
+            </p>    
+                    </div>
+                <div>
+                    <img 
+                    src="Images/Welcome to sheffield .jpg"
+                    alt="Image of the city">
+                </div>
+                </section>
+
+                <div class="middleParagraph">
+                    <p>
+                        Cantor College was established in 
+                        1989 to specialize in Computing and 
+                        Design.   At Cantor College, we want
+                        to give students the education they 
+                        need to achieve their career aims, 
+                        leaving them equipped with the 
+                        knowledge, skills and experience to 
+                        succeed.     
+                    </p>
+                </div>
+
+                <section>
+                   <div>
+                    <img
+                    src="./Images/cantor-gallery.jpg"
+                    alt="Image of the College">
+                   </div>
+
+                   <div>
+                    <p>
+                        Cantor College gives you the opportunities 
+                        that can give you the edge when you enter 
+                        the world of work, through our teaching 
+                        and our links to some of the world's 
+                        #leading researchers and employers. Our 
+                        students have gone on to successful careers 
+                        in a wide range of industries across the 
+                        globe.   
+                    </p>
+                   </div> 
+                </section>
+
+                <div class="lastParagraph"> 
+                    <p>
+                        Whatever your ambitions, our learning and 
+                        support can help to get the most out of 
+                        your time with Cantor College, both as a 
+                        student and in your future career. 
+                    </p>
+                </div>
+            
+
+            </main>
+
+        </div>
+   
+        <footer>
+            <p>© 2024 Cantor College 
+            </p>
+        </footer>
+
+    </body>
+</html>
